ed29cdfc346d7768204efb84ffde8f9897cc42ef
UlisesIslas <20193tn013@utez.edu.mx>
2022-09-20 10:21:12 -0500

    1St Commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    console.log("SW: Install");
+});
+
+self.addEventListener('fetch', (event) => {
+    /* if (event.request.url.includes('css')) {
+        console.log(event.request.url);
+        const res = new Response(`
+        body {
+            color:red;
+            background-color: black;
+        }`,{headers: {
+            'Content-Type':'text/css'
+        }});
+        event.respondWith(res);
+    }  */
+    if (event.request.url.includes('jpg')) {
+        event.respondWith(fetch('/img/gato.png'));
+    }
+});
